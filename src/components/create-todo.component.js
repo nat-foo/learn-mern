1143e7b218d6a5ae6ec9bc9afa39c89cf69e1934
SupernaturalCow <inaniloquaint@gmail.com>
2020-04-24 16:10:11 +1000

    Completed 'Create The Basic Layout and Navigation'

---
+++ src/components/create-todo.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class CreateTodo extends Component
+{
+    render() {
+        return (
+            <div>
+                <p>Welcome to the CreateTodo component!</p>
+            </div>
+        )
+    }
+}
